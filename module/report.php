bf974feee8dc500d79063de33f1324986fd5f103
satyammohan <satyammohan@gmail.com>
2022-06-09 01:08:19 -0400

    live changes

--- module/report.php
+++ module/report.php
@@ -6,7 +6,7 @@ class report extends common {
         parent:: __construct();
     }
     function vehicledetail() {
-        $_REQUEST['start_date'] = $sdate = isset($_REQUEST['start_date']) ? $_REQUEST['start_date'] : date("Y-m-01");
+        $_REQUEST['start_date'] = $sdate = isset($_REQUEST['start_date']) ? $_REQUEST['start_date'] : date("Y-04-01");
         $_REQUEST['end_date'] = $edate = isset($_REQUEST['end_date']) ? $_REQUEST['end_date'] : date("Y-m-d");
 
 
@@ -16,7 +16,6 @@ class report extends common {
 
         $sql = "SELECT b.*, a.name AS aname, c.name AS cname FROM {$this->prefix}billdet b, {$this->prefix}company c, {$this->prefix}area a
             WHERE (b.date >= '$sdate' AND b.date <= '$edate') AND b.vehno='$vehno' AND b.id_from_area=a.id_area AND b.id_company=c.id_company ORDER BY date";
-        echo $sql;
         $data = $this->m->sql_getall($sql);
         $this->sm->assign("data", $data);
     }
